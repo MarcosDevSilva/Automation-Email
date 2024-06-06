b9f39cb2dfb625606945cf7a5c7f8f52c41452e5
Marcos <tassio50000@gmail.com>
2024-06-06 16:35:15 -0300

    finalizando projetinho de automação

--- automation.py
+++ automation.py
@@ -1,35 +1,74 @@
 import yfinance
 import pyautogui
-# import pyperclip
+import pyperclip
 import webbrowser
 import time
 
-destianatario = "tassio50000@gmail.com"
-assunto = "testando email"
-mensagem = "marcao lindao"
 
-# ticker = input("Digite o codigo da ação: ")
+ticker = input("Digite o codigo da ação: ")
 
-dataYf = yfinance.Ticker("PETR4.SA").history(start="2023-01-01", end=("2023-12-31"))
+dataYf = yfinance.Ticker(ticker).history(start="2023-01-01", end=("2023-12-31"))
 fechamento =dataYf.Close
 
 cotacaoMaxima = round(fechamento.max(), 2)
 cotacaoMinima = round(fechamento.min(), 2)
 media = round(fechamento.mean(), 2)
 
+destianatario = input("Digite o E-mail do destinatario: ")
+assunto = "testando email"
+mensagem = f"""
+Aqui estão os dados q vc me pediu,
+
+Cotações da ação:{ticker}
+
+Cotaçao maxima:    R${cotacaoMaxima}
+Cotaçõa minima:    R${cotacaoMinima}
+Media de Cotações :R${media}
+
+tmj.
+"""
+
 webbrowser.open("www.gmail.com")
 time.sleep(3)
 
+# config uma pausa de 3 segundos
+pyautogui.PAUSE = 3
+
 pyautogui.click(x=2023, y=225)
-time.sleep(10)
-
-clicar = pyautogui.position()
 
 
+# ecolher o destinatario
+pyperclip.copy(destianatario)
+pyautogui.hotkey("ctrl", "v")
+pyautogui.hotkey("tab")
 
-print(cotacaoMaxima)
-print(cotacaoMinima)
-print(media)
-print(clicar)
+# digitar o assunto
+pyperclip.copy(assunto)
+pyautogui.hotkey("ctrl", "v")
+pyautogui.hotkey("tab")
+
+# digitar a msg
+pyperclip.copy(mensagem)
+pyautogui.hotkey("ctrl", "v")
+
+# clicar no botão enviar
+pyautogui.click(x=3220, y=1050)
+
+# fechar  aba do email
+pyautogui.hotkey("ctrl", "f4")
+
+print("Email enviado corretamente")
+
+
+# achar as coordenadas da tela
+# time.sleep(2)
+# clicar = pyautogui.position()
+
+
+
+# print(cotacaoMaxima)
+# print(cotacaoMinima)
+# print(media)
+# print(clicar)
 
 
